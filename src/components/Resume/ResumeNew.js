299c0472d37d3e75bcd2a6a4600adc31dad8789d
Angel Camargo <adcamargo10@gmail.com>
2023-02-19 20:56:08 -0400

    Changing Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Resume-Angel-Camargo.pdf";
+import pdf from "../../Assets/../Assets/CV-Español-AngelCamargo.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/AngelCamargo404/portfolio-camargo/main/src/Assets/Resume-Angel-Camargo.pdf";
+  "https://raw.githubusercontent.com/AngelCamargo404/portfolio-camargo/main/src/Assets/CV-Español-AngelCamargo.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
