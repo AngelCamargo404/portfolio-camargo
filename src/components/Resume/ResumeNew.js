4b0ae85f63787f2834b0b5f20618a1c333b46b6a
Angel Camargo <adcamargo10@gmail.com>
2023-02-19 20:59:03 -0400

    Changing Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV-Español-Angel-Camargo.pdf";
+import pdf from "../../Assets/../Assets/CV-English-Angel-Camargo.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
